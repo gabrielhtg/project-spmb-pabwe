ee12cc943307bcf6315c0544e25c41b5f7d2a97a
Jayfline <jayfline7@gmail.com>
2023-11-24 11:54:14 +0700

    Menambahkan konten pengumuman

--- resources/views/pengumuman/pengumuman.blade.php
+++ resources/views/pengumuman/pengumuman.blade.php
@@ -1,11 +1,21 @@
 @extends('template.app')
 
+@section('head')
+    <link rel="stylesheet" href="{{ asset('assets/css/pengumuman.css') }}">
+@endsection
+
 @section('isi-halaman')
 
-    <section class="container" id="Pengumuman">
-      <div class="hero-pengumuman">
-        <h1 class="judul-pengumuman">Pengumuman SPMB</h1>
-      </div>
-    </section>
+  <head>
+  <link rel="stylesheet" href="{{ asset('assets/css/pengumuman.css') }}">
+  </head>
+
+  <body>
+  <section class="container" id="Pengumuman">
+        <div class="hero-pengumuman">
+          <h1 class="judul-pengumuman">Pengumuman SPMB</h1>
+        </div>
+      </section>
+  </body>
 
 @endsection
