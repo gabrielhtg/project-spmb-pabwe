a884c09e6e52b4c83f11174c1716149000ca9174
Hanny Yosephine <81764285+hannyyosephine16@users.noreply.github.com>
2023-11-24 08:18:00 +0700

    menambahkan laman fasilitas olahraga dan kesehatan

--- resources/views/fasilitas/fasilitasKesehatandanOlahraga.blade.php
+++ resources/views/fasilitas/fasilitasKesehatandanOlahraga.blade.php
@@ -2,4 +2,84 @@
 
 @section('isi-halaman')
 
+    <section class="container" id="Olahraga dan Kesehatan">
+        <div class="text-center fw-bolder">
+            <h1>Olahraga & Kesehatan</h1>
+        </div>
+        <div class="custom-justified-text align-items-center pt-3">
+          <p>
+            Kampus IT Del memiliki fasilitas dibagian olahraga dan kesehatan. Fasilitas dibagian olahraga terdapat beberapa lapangan yang dipakai 
+          </p>
+            <p>Sejak didirikan tahun 2001, IT Del sudah mengharuskan mahasiswanya untuk tinggal diasrama tanpa terkecuali.  
+            Tujuannya adalah agar menunjang keberhasilan pencapaian tujuan pendidikan mahasiswanya. Asrama di IT Del juga merupakan sarana pendidikan dalam pembentukan karakter mahasiswanya.
+            IT Del selalu berusaha menciptakan manusia yang tidak hanya pintar tapi juga yang MarTuhan, Maroha dan Marbisuk (Berketuhanan, Berperasaan dan Bijaksana).</p>
+        </div>
+        
+        <div class="container pt-5" id="lapangan hijau">
+            <div class="row">
+                <div class="col-md-6">
+                <img src="{{ asset('/assets/img/fasilitas/lapangan hijau.jpg') }}" class="img-fluid custom-image" alt="lapangan-hijau">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>Lapangan Hijau</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                        Lapangan Hijau adalah salah satu fasilitas yang ada di IT Del. Lapangan hijau biasa digunakan oleh warga Del untuk melakukan acara-acara besar baik itu acara formal maupun non-formal. Lapangan hijau biasanya digunakan oleh mahasiswa untuk kegiatan dari himpunan prodi seperti temu ramah bersama, welcoming party, atau acara dari BEM seperti festival ataupun acara lainnya.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container pt-5" id="lapangan basket">
+            <div class="row">
+                <div class="col-md-6">
+                <img src="{{ asset('/assets/img/fasilitas/lapangan basket.jpeg') }}" class="img-fluid custom-image" alt="lapangan-basket">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>Lapangan Basket</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                        Lapangan basket adalah lapangan yang juga merupakan fasilitas bagi mahasiswa dibidang olahraga. Lapangan basket digunakan untuk acara olahraga yang disediakan oleh BEM seperti KM CUP (Kegiatan Mahasiswa CUP), dan lapangan ini juga biasa digunakan oleh mahasiswa untuk mengisi waktu luang di weekend atau di sore hari setelah jam akademik.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container pt-5" id="lapangan voli">
+            <div class="row">
+                <div class="col-md-6">
+                <img src="{{ asset('/assets/img/fasilitas/lapangan voli.jpg') }}" class="img-fluid custom-image" alt="lapangan-voli">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>Lapangan Voli</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                        Lapangan Voli tidak  jauh berbeda fungsinya dengan lapangan basket. Lapangan voli juga digunakan dalam acara olahraga BEM dan digunakan disaat waktu luang.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container pt-5" id="klinik IT Del">
+            <div class="row">
+                <div class="col-md-6">
+                <img src="{{ asset('/assets/img/fasilitas/klinik.jpg') }}" class="img-fluid custom-image" alt="klinik_IT Del">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>klinik IT Del</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                        Klinik adalah fasilitas kesehatan yang dimiliki oleh IT Del. Klinik IT Del dilengkapi dengan obat-obat yang lengkap, ruangan yang diperlukan jika ada mahasiswa yang perlu diinfus, dokter dan juga perawat.  Selain itu juga tersedianya mobil kesehatan yang digunakan untuk menjemput mahasiswa sakit yang berada dikampus.
+                        </p>    
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
