9e68735526fc0368c340e6f4645bc0078d39870f
ifs21045-itdel <ifs21045@student.del.ac.id>
2023-11-24 09:47:36 +0700

    mengisi laman fasilitas-layanan makanan

--- resources/views/fasilitas/fasilitasLayananMakanan.blade.php
+++ resources/views/fasilitas/fasilitasLayananMakanan.blade.php
@@ -1,5 +1,94 @@
 @extends('template.fasilitas')
 
 @section('isi-halaman')
+    <section class="container" id="LayananMakanan">
+        <div class="text-center fw-bolder">
+            <h1>Layanan Makanan</h1>
+        </div>
+        <div class="custom-justified-text align-items-center pt-3">
+            <p>Penyedia layanan makanan di kampus menyediakan beragam makanan dan minuman untuk mahasiswa dan staf kampus.
+                Mereka dapat berupa kantin atau kafe fisik di area kampus atau menggunakan platform daring. Layanan ini
+                bertujuan
+                menyediakan pilihan makanan yang sehat dan bervariasi untuk memenuhi kebutuhan nutrisi komunitas kampus.
+                Beberapa penyedia mungkin menekankan nilai-nilai berkelanjutan dengan penggunaan bahan organik atau
+                pengemasan ramah lingkungan.</p>
+        </div>
 
+        <div class="container pt-5" id="asrama-pniel">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="{{ asset('/assets/img/fasilitas/layanan-kantin.jpg') }}" class="img-fluid custom-image"
+                        alt="layanan-kantin">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>Kantin</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                            Institut Teknologi Del memiliki beberapa kanti, diantaranya adalah:
+                        <ol>
+                            <li>Kantin Baru</li>
+                            <ul>
+                                <li> Kantin Baru lantai 1 atas</li>
+                                <li> Kantin Baru lantai 1 bawah</li>
+                                <li> Kantin Baru lantai 2 Atas</li>
+                                <li> Kantin Baru lantai 2 bawah</li>
+                                <li> Kantin Tengah Atas</li>
+                                <li> Kantin Tengah Bawah</li>
+                            </ul>
+                            <li>Kantin Lama</li>
+                            <ul>
+                                <li> Kantin Lama lantai 1</li>
+                                <li> Kantin Lama lantai2</li>
+                            </ul>
+                        </ol>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container pt-5" id="layanan-koperasi">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="{{ asset('/assets/img/fasilitas/layanan-koperasi.jpg') }}" class="img-fluid"
+                        alt="layanan-koperasi">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>Koperasi</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                            Koperasi Institut Teknologi Del adalah tempat untuk membeli kebutuhan-kebutuhan mahasiswa.
+                            Koperasi menyediakan berbagai kebutuhan mahasiswa del baik itu kebutuhan belajar, kebutuhan
+                            kesehatan, dan kebutuhan primer dari mahasiswa Institut Teknologi Del.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container pt-5" id="layanan-cafetaria">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="{{ asset('/assets/img/fasilitas/layanan-cafetaria.jpg') }}" class="img-fluid"
+                        alt="layanan-cafetaria">
+                </div>
+
+                <div class="col-md-6 mx-auto d-flex flex-column my-auto">
+                    <h3>Cafetaria</h3>
+                    <div class="lh-base text-break text-justify custom-justified-text align-items-center">
+                        <p>
+                            Kafetaria (atau dalam bahasa Inggris "cafeteria") adalah jenis restoran atau tempat makan yang
+                            memiliki ciri khas tertentu dalam penyajian makanan. Ciri khas utamanya adalah bahwa makanan dan
+                            minuman disajikan di berbagai meja atau loket yang tersusun di dalam ruangan, dan pelanggan
+                            dapat memilih makanan mereka sendiri dari berbagai pilihan yang disajikan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    </main>
 @endsection
