d271e4479e370ebe4fc2a272eddbfc925c699227
Jayfline <jayfline7@gmail.com>
2023-11-25 21:04:42 +0700

    Menambahkan konten pengumuman

--- resources/views/pengumuman/pengumuman.blade.php
+++ resources/views/pengumuman/pengumuman.blade.php
@@ -1,21 +1,22 @@
 @extends('template.app')
 
-@section('head')
-    <link rel="stylesheet" href="{{ asset('assets/css/pengumuman.css') }}">
-@endsection
-
 @section('isi-halaman')
+    <head>
+        <link rel="stylesheet" href="{{ asset('/assets/css/pengumuman.css') }}">
+    </head>
 
-  <head>
-  <link rel="stylesheet" href="{{ asset('assets/css/pengumuman.css') }}">
-  </head>
-
-  <body>
-  <section class="container" id="Pengumuman">
+    <body>
+        <section class="container-judul" id="Pengumuman">
         <div class="hero-pengumuman">
-          <h1 class="judul-pengumuman">Pengumuman SPMB</h1>
+            <h1 class="judul-pengumuman">Pengumuman SPMB</h1>
+            <p>
+                Halaman website ini adalah tempat resmi untuk mendapatkan informasi terbaru seputar asrama, pengumuman kelulusan
+                ujian masuk, jadwal kegiatan, dan berita penting lainnya. Cari informasi dengan mudah melalui fitur pencarian
+                atau berdasarkan tag yang disediakan.
+            </p>
         </div>
-      </section>
-  </body>
+        </section>
 
+
+    </body>
 @endsection
