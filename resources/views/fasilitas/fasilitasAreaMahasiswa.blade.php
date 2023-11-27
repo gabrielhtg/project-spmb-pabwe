2326eeb6bd1661db03807bfcd7f1b8006fa0fe44
ifs21049-itdel <ifs21049@students.del.ac.id>
2023-11-27 14:17:51 +0700

    menambahkan asset area mahasiswa

--- resources/views/fasilitas/fasilitasAreaMahasiswa.blade.php
+++ resources/views/fasilitas/fasilitasAreaMahasiswa.blade.php
@@ -30,7 +30,7 @@
           <div class="container pt-5" id="perpustakaan">
             <div class="row">
                 <div class="col-md-6">
-                <img src="{{ asset('/assets/img/fasilitas/asrama-pniel.jpeg') }}" class="img-fluid custom-image" alt="asrama-pniel">
+                <img src="{{ asset('/assets/img/fasilitas/perpustakaan.jpg') }}" class="img-fluid custom-image" alt="asrama-pniel">
                 </div>
 
                 <div class="col-md-6 mx-auto d-flex flex-column my-auto">
