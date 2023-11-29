55c6ffa0cca8b5045e2cfee9823052834826232b
agustinaveronika <agustinabutarbutar168@gmail.com>
2023-11-29 11:47:49 +0700

    menambah navigasi admin

--- resources/views/template/admin-panel-template.blade.php
+++ resources/views/template/admin-panel-template.blade.php
@@ -44,6 +44,18 @@
                         Dashboard Panel
                     </button>
                 </a>
+                <a href="#">
+                    <button id="btn_dashboard_panel" class="btn_sidebar w-100 text-start">
+                        <i class="fa-regular fa-handshake"></i>
+                        Mitra
+                    </button>
+                </a>
+                <a href="#">
+                    <button id="btn_dashboard_panel" class="btn_sidebar w-100 text-start">
+                        <i class="bi bi-question-square"></i>
+                       FAQ
+                    </button>
+                </a>
 
             </div>
         </div>
@@ -64,6 +76,12 @@
                     <button id="btn_akreditasi" class="btn_sidebar_kecil">A</button>
                     <a href="{{ route("spmb-dashboard") }}">
                         <button id="btn_dashboard_panel" class="btn_sidebar_kecil"><i class="bi bi-house"></i></button>
+                    </a>
+                    <a href="{{ route("spmb-dashboard") }}">
+                        <button id="btn_dashboard_panel" class="btn_sidebar_kecil"><i class="fa-regular fa-handshake"></i></button>
+                    </a>
+                    <a href="{{ route("spmb-dashboard") }}">
+                        <button id="btn_dashboard_panel" class="btn_sidebar_kecil"><i class="bi bi-question-square"></i></button>
                     </a>
                 </div>
             </div>
