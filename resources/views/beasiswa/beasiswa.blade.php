90b4ca4f1e55177eb1adc0f20becb88c03c2711b
ifs21055-itdel <ifs21055@students.del.ac.id>
2023-11-29 15:34:53 +0700

    menambahkan halaman beasiswa

--- resources/views/beasiswa/beasiswa.blade.php
+++ resources/views/beasiswa/beasiswa.blade.php
@@ -3,12 +3,10 @@
 @section('isi-halaman')
 
 <section id="hero" class="container">
-    <title>Daftar Beasiswa</title>
     <link rel="stylesheet" href="{{ asset('assets/css/beasiswa.css') }}" />
-    <img src="{{ asset('/assets/img/beasiswa/beasiswa-wp.jpg') }}" class="d-block w-100 blur-image" alt="gambar-1">
-    <div class="carousel-caption d-none d-md-block">
-        <div class="d-flex flex-column justify-content-center h-100 text-start margin-hero-title">
-            <h1 class="display-5 fw-bold">Beasiswa IT Del</h1>
+    <div class="masthead" style="background-size: cover; min-height: 75vh; position: relative; color: #ffff; text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2); background-image: url('{{ asset('assets/img/beasiswa/beasiswa-wp.jpg') }}');">
+        <div class="color-overlay d-flex justify-content-center align-items-center">
+          <h2 class="beasiswa-text" style="height: 90px">Beasiswa IT Del</h2>
         </div>
     </div>
 </section>
