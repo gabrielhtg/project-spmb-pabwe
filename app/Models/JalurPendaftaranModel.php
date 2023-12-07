ca77b06132a50ca83d02aae3146ca9e0b74a77dd
TiaraYu8 <yuliandari.tiara@gmail.com>
2023-12-08 01:21:02 +0700

    addJalur

--- app/Models/JalurPendaftaranModel.php
+++ app/Models/JalurPendaftaranModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ModelHeaderAdmisi extends Model
+class JalurPendaftaranModel extends Model
 {
     use HasFactory;
 
