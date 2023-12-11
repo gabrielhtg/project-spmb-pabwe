5c9ed1c7284a0ece81ba4e25d668dfd4490395d2
Jayfline <jayfline7@gmail.com>
2023-12-11 15:03:00 +0700

    Mengupdate pesan error pada bagian fasilitas pada laman admin

---
+++ resources/lang/id/validation.php
@@ -0,0 +1,12 @@
+<?
+
+return [
+    'required' => 'Kolom :attribute wajib diisi.',
+    'file_gambar' => [
+        'required' => 'File gambar wajib diunggah.',
+        'image' => 'File harus berupa gambar.',
+        'mimes' => 'Kolom :attribute wajib diisi dengan format file jpeg, jpg, atau png.',
+        'max' => 'Ukuran file tidak boleh lebih dari 2 MB.',
+    ],
+    // Tambahkan pesan validasi lainnya sesuai kebutuhan
+];
