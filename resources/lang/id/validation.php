25aaac07af014d09d3e59926f7863707a8b108a2
Jayfline <jayfline7@gmail.com>
2023-12-11 17:13:04 +0700

    Mengupdate pesan error pada laman admin untuk bagian penambahan/edit konten fasilitas dan pengumuman

--- resources/lang/id/validation.php
+++ resources/lang/id/validation.php
@@ -1,11 +1,14 @@
-<?
+<?php
 
 return [
     'required' => 'Kolom :attribute wajib diisi.',
     'file_gambar' => [
         'required' => 'File gambar wajib diunggah.',
-        'image' => 'File harus berupa gambar.',
-        'mimes' => 'Kolom :attribute wajib diisi dengan format file jpeg, jpg, atau png.',
-        'max' => 'Ukuran file tidak boleh lebih dari 2 MB.',
+        'image' => 'File :attribute harus berupa gambar.',
+        'mimes' => 'File :attribute wajib diisi dengan format file jpeg, jpg, atau png.',
+        'max' => 'Ukuran file :attribute tidak boleh lebih dari 2 MB.',
+    ],
+    'filePengumuman' => [
+        'mimes' => 'File :attribute wajib diisi dengan format file PDF.',
     ],
 ];
