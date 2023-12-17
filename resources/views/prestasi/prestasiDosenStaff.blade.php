c97edff816c7525689a0dab7e874d8729543c4c7
11S21048 <ifs21048@students.del.ac.id>
2023-12-17 16:20:54 +0700

    memperbaiki scroll bar pada card prestasi, sudah bisa berfungsi

--- resources/views/prestasi/prestasiDosenStaff.blade.php
+++ resources/views/prestasi/prestasiDosenStaff.blade.php
@@ -2,145 +2,23 @@
 
 @section('isi-halaman')
 <style>
-        .cat-prestasi {
-            margin-top: 20px;
-            margin-bottom: 70px;
-        }
-
-        body {
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        h2 {
-            padding-bottom: 5px;
-            text-align: center;
-        }
-
-        .card {
+    .card {
             margin-bottom: 30px;
-            width:300px;
-            height:450px
+            /* width:300px;
+            height:450px */
+            height:95%;
         }
+    
+    .mx-auto{
+        margin-bottom: 30px;
+        margin-top: 20px;
+    }
 
-        .card-text {
-            overflow: hidden;
-            text-overflow: ellipsis;
-            display: -webkit-box;
-            -webkit-line-clamp: 3; /* Jumlah baris yang ingin ditampilkan */
-            -webkit-box-orient: vertical;
-        }
-
-        .popup {
-            display: none;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            /* background-color: white; */
-            backdrop-filter: blur(5px);
-            z-index: 1000;
-        }
-
-        .popup-content {
-            position: absolute;
-            width: 650px;
-            height:700px;
-            top: 52%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            padding: 20px;
-            background-color: #fff;
-            /* White background */
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-            overflow-y: auto; /* Tambahkan overflow-y agar dapat discroll jika kontennya panjang */
-        }
-
-        .popup .popup-content img {
-            width: 100%;
-            /* Image takes 100% width of its container */
-            height: 330px;
-            display: block;
-            margin-top: 40px;
-        }
-
-        .overlay {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            z-index: 1000;
-        }
-
-
-        .btn-close {
-            cursor: pointer;
-            background-color: #007bff;
-            /* Button background color */
-            color: #fff;
-            /* Button text color */
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            font-size: 16px;
-            margin-right: 10px;
-        }
-
-        .btn-close:hover {
-            background-color: #0056b3;
-            /* Button background color on hover */
-        }
-
-
-        .popup-content img {
-            width: 100%;
-            height: 330px;
-        }
-
-        .content-text {
-            margin-top: 20px;
-            /* white-space: pre-wrap; Agar konten dapat mempertahankan format dan baris baru */
-        }
-
-        .btn-close {
-            background-color: #4caf50;
-            /* Solid background color */
-            color: white;
-            padding: 10px 15px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            position: absolute;
-            top: 10px;
-            right: 10px;
-            z-index: 1001;
-        }
-
-        .btn-close:hover {
-            background-color: #45a049;
-        }
-
-        .popup.active {
-            display: block;
-        }
-
-        .popup-footer {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin-top: 10px;
-        }
-
-        .popup-date {
-            font-size: 14px;
-            color: black;
-            /* Adjust the color to your preference */
-        }
-    </style>
-
+    .modal-content .modal-body img {
+        width:100%;
+        height:250px;
+    }
+</style>
     <section class="container">
         <div class="cat-prestasi">
             {{-- <div class="d-flex justify-content-between align-items-center">
@@ -159,32 +37,30 @@
                                         {{ substr($item->deskripsi, 0, 154) }}
                                     </p>
                                     <div class="d-flex justify-content-between align-items-center">
-                                        <p style="font-size: 14px; margin: 0;">{{$item->created_at}}</p>
-                                        <a href="#" class="btn btn-primary open-popup" data-popup="#popup{{$item->id}}">See
-                                            more</a>
-                                    </div>
+                                    <p style="font-size: 14px; margin: 0;">{{$item->created_at}}</p>
+                                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#popup{{$item->id}}">See More</button>
+                                </div>
 
-                                    <div class="popup" id="popup{{$item->id}}">
-                                        <div class="overlay"></div>
-                                        <div class="popup-content">
-                                            <img
-                                                src="{{asset($item->photo)}}"
-                                                class="card-img-top"
-                                                alt="pict1"
-                                            />
-
-                                            <div class="content-text">
+                                <div class="modal fade" id="popup{{$item->id}}" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modal-title">Prestasi Dosen/Staff</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <img src="{{ asset($item->photo) }}" class="card-img-top" alt="pict1"/>
                                                 <h5 class="card-title">{{$item->judul_prestasi}}</h5>
-                                                <p class="card-text">
+                                                <p>
                                                     {{$item->deskripsi}}
                                                 </p>
                                             </div>
-                                            <div class="popup-footer">
-                                                <span class="popup-date">{{$item->created_at}}</span>
-                                                <button class="btn-close"></button>
+                                            <div class="modal-footer">
+                                                <p style="font-size: 14px; margin: 0;">{{$item->created_at}}</p>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
                                 </div>
                             </div>
                         </div>
