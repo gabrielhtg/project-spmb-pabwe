4a722b9c7e73141f6e0cedb7f313c99df7289187
ifs21055-itdel <ifs21055@students.del.ac.id>
2023-12-20 09:02:42 +0700

    menambahkan seeder beasiswa

--- database/seeders/add_beasiswa.php
+++ database/seeders/add_beasiswa.php
@@ -15,34 +15,64 @@ class add_beasiswa extends Seeder
     {
         DB::table('beasiswa')->insert([
             [
-                'judul' => '',
-                'deskripsi' => '',
-                'gambar' => '',
-                'file' => '',
+                'judul' => 'Beasiswa Prestasi Per Semester ( IPS )',
+                'deskripsi' => 'Beasiswa IPS diberikan kepada mahasiswa dalam bentuk potongan SPP dengan aturan
+                sebagai berikut :
+                a. Beasiswa Prestasi Per Semester (IPS) diberikan bagi semua mahasiswa pada tingkat
+                1, tingkat 2, tingkat 3 dan tingkat 4 kecuali mahasiswa pada semester terakhir masa
+                studinya (semester 6 pada prodi DIII dan semester 8 pada prodi DIV dan S1).
+                b. Mahasiswa penerima Beasiswa Prestasi harus memiliki perilaku terpuji dan dibuktikan dengan Nilai Perilaku minimal AB. ',
+                'gambar' => 'assets/img/beasiswa/img/beasiswa.jpg',
+                'file' => '/assets/img/beasiwa/pdf/beasiswa.pdf',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'judul' => '',
-                'deskripsi' => '',
-                'gambar' => '',
-                'file' => '',
+                'judul' => 'Penghargaan ”Best Performance”',
+                'deskripsi' => 'Penghargaan ”Best Performance”
+                a. Penghargaan “Best Performance” adalah penghargaan yang diberikan kepada
+                mahasiswa dengan kenaikan IPS paling tinggi antara IP semester ganjil dan IP
+                semester genap pada tiap tahun ajaran.
+                b. Penghargaan “Best Performance” diberikan kepada satu orang mahasiswa pada
+                setiap angkatan (tingkat).',
+                'gambar' => 'assets/img/beasiswa/img/beasiswa.jpg',
+                'file' => 'assets/beasiwa/pdf/beasiswa.pdf',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'judul' => '',
-                'deskripsi' => '',
-                'gambar' => '',
-                'file' => '',
+                'judul' => 'Beasiswa Potensi Cum Laude dan Cum Laude',
+                'deskripsi' => 'Mahasiswa
+                penerima Beasiswa Potensi Cum Laude harus memiliki perilaku terpuji dan dibuktikan
+                dengan Nilai Perilaku minimal AB (aturan mengenai nilai perilaku akan dikeluarkan
+                oleh bagian kemahasiswaan). Bagi mahasiswa yang sedang menerima beasiswa dari
+                pihak lain tidak dapat menerima beasiswa Potensi Cum Laude',
+                'gambar' => 'assets/img/beasiswa/img/beasiswa.jpg',
+                'file' => 'assets/beasiwa/pdf/beasiswa.pdf',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'judul' => '',
-                'deskripsi' => '',
-                'gambar' => '',
-                'file' => '',
+                'judul' => 'Penghargaan “Mahasiswa Teladan”',
+                'deskripsi' => 'Mahasiswa yang mendapatkan predikat Mahasiswa Teladan akan mendapatkan Voucher
+                Potongan SPP sebesar 100%. Mahasiswa penerima Penghargaan “Mahasiswa Teladan”
+                harus memiliki perilaku terpuji dan dibuktikan dengan Nilai Perilaku minimal AB
+                (aturan mengenai nilai perilaku akan dikeluarkan oleh bagian kemahasiswaan).',
+                'gambar' => 'assets/img/beasiswa/img/beasiswa.jpg',
+                'file' => 'assets/beasiwa/pdf/beasiswa.pdf',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'judul' => 'Penghargaan “Lulusan Terbaik”',
+                'deskripsi' => 'Tiga lulusan yang meraih Indeks Prestasi Kumulatif (IPK) tertinggi dari setiap Program
+                Studi pada setiap akhir tahun kelulusan akan mendapatkan penghargaan berupa bebas
+                uang wisuda dan medali sebagai lulusan terbaik 1, 2, dan 3 dari masing-masing prodi
+                berdasarkan IPK tertinggi di masing-masing prodi. Selain IPK lulusan juga harus
+                memiliki perilaku terpuji dan dibuktikan dengan Nilai Perilaku minimal AB (aturan
+                mengenai nilai perilaku akan dikeluarkan oleh bagian kemahasiswaan).',
+                'gambar' => 'assets/img/beasiswa/img/beasiswa.jpg',
+                'file' => 'assets/beasiwa/pdf/beasiswa.pdf',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
--- resources/views/beasiswa/beasiswa.blade.php
+++ resources/views/beasiswa/beasiswa.blade.php
@@ -2,48 +2,34 @@
 
 @section('isi-halaman')
 
-<!-- <section id="hero" class="container">
-    <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{ asset('assets/img/beasiswa/beasiswa-wp.jpg') }}" class="d-block w-100" alt="...">
-            </div>
-        </div>
-    </div>
-</section> -->
-
 <section>
     <div class="masthead" style="background-size: cover; min-height: 75vh; position: relative; color: #ffff; text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2); background-image: url('{{ asset('assets/img/beasiswa/beasiswa-wp.jpg') }}');">
         <div class="color-overlay d-flex justify-content-center align-items-center">
-        <link rel="stylesheet" href="{{ asset('assets/css/kel5.css') }}" />
-          <h2 class="beasiswa-text" style="height: 90px">Beasiswa</h2>
+            <link rel="stylesheet" href="{{ asset('assets/css/kel5.css') }}" />
+            <h2 class="beasiswa-text" style="height: 90px">Beasiswa</h2>
         </div>
     </div>
 </section>
 
-    @foreach($beasiswa as $beasiswas)
-    <div class="container mt-3">
-        <div class="card mb-3">
-            <h5 class="card-header text-center" style="height: 56.7px; width: 100%; display: flex; align-items: center; justify-content: center; background-color: #d9d9d9; color: #0066cc; font-weight: bold;">{{$beasiswas->judul}}</h5>
-            <div class="row g-0">
-                <div class="col-md-4">
+@foreach($beasiswa as $beasiswas)
+<div class="container mt-3">
+    <div class="card mb-3">
+        <h5 class="card-header text-center" style="height: 56.7px; width: 100%; display: flex; align-items: center; justify-content: center; background-color: #d9d9d9; color: #0066cc; font-weight: bold;">{{$beasiswas->judul}}</h5>
+        <div class="row g-0">
+            <div class="col-md-4">
                 <img src="{{ asset($beasiswas->gambar) }}" class="img-fluid rounded-start" style="height: 100%; object-fit: cover;" alt="...">
-
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <div class="card-text" style="text-align:justify; padding-right:20px"> 
-                            <p>{{ $beasiswas->deskripsi }}</p>
-                            <a href="{{ asset($beasiswas->file) }}" class="btn btn-primary" style="position: absolute; bottom: 10px; right: 10px;" download>Download PDF</a>
-                        </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body" style="height: 100%;"> <!-- Tambahkan style height: 100% di sini -->
+                    <div class="card-text" style="text-align:justify; padding-right:20px; height: 100%;"> <!-- Tambahkan style height: 100% di sini -->
+                        <p>{{ $beasiswas->deskripsi }}</p>
+                        <a href="{{ asset($beasiswas->file) }}" class="btn btn-primary" style="position: absolute; bottom: 10px; right: 10px;" download>Download PDF</a>
                     </div>
-                    <div>
                 </div>
             </div>
         </div>
     </div>
-    @endforeach
-
-
+</div>
+@endforeach
 
 @endsection
